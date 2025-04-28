4c6543b49153b7e52621e678d59ff82bad4b8439
HamdanRaza309 <githubuse309@gmail.com>
2025-04-28 07:48:05 +0500

    Allow methods and headers in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,12 +7,17 @@ const upload = multer()
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    origin: ['https://snap-gallery-eta.vercel.app', 'http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+}));
+
+
 app.use(express.json({
     limit: '16kb'
 }))
+
 app.use(express.urlencoded({
     extended: true,
     limit: '16kb'
